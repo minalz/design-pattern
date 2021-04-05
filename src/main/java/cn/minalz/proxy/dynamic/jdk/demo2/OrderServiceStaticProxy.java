e25363e1862339b5e8c856c3a0ff90af37c05b10
minalz <119687281@qq.com>
2021-04-06 00:50:14 +0800

    添加静态代理demo

---
+++ src/main/java/cn/minalz/proxy/dynamic/jdk/demo2/OrderServiceStaticProxy.java
@@ -0,0 +1,40 @@
+package cn.minalz.proxy.dynamic.jdk.demo2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @description: 创建切换数据源的代理类
+ * @author: minalz
+ * @date: 2021-04-06 00:39
+ **/
+public class OrderServiceStaticProxy implements IOrderService {
+
+    private SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+
+    private IOrderService orderService;
+
+    public OrderServiceStaticProxy(IOrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @Override
+    public int createOrder(Order order) {
+        before();
+        Long time = order.getCreateTime();
+        int dbRouter = Integer.parseInt(yearFormat.format(new Date()));
+        System.out.println("静态代理类自动分配到【DB_" + dbRouter + "】数据源处理数据");
+        DynamicDataSourceEntry.set(dbRouter);
+        orderService.createOrder(order);
+        after();
+        return 0;
+    }
+
+    private void before() {
+        System.out.println("Proxy before method.");
+    }
+
+    private void after() {
+        System.out.println("Proxy after method.");
+    }
+}
---
+++ src/main/java/cn/minalz/proxy/dynamic/jdk/demo2/Test4.java
@@ -0,0 +1,36 @@
+package cn.minalz.proxy.dynamic.jdk.demo2;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @description:
+ * @author: minalz
+ * @date: 2021-04-06 00:44
+ **/
+public class Test4 {
+
+    public static void main(String[] args) {
+        try {
+            Order order = new Order();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+            Date date = sdf.parse("2021/04/06");
+            order.setCreateTime(date.getTime());
+
+            OrderServiceStaticProxy orderService = new OrderServiceStaticProxy(new OrderService());
+            orderService.createOrder(order);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 执行结果如下:
+    /**
+     * Proxy before method.
+     * 静态代理类自动分配到【DB_2021】数据源处理数据
+     * OrderService调用orderDao创建订单
+     * OrderDao插入Order成功
+     * Proxy after method.
+     */
+}
